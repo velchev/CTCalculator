4268d9ba80eb3a918b936021c2d9598ac827387f
Lyubomir Velchev <lyubomir.velchev@gmail.com>
2020-12-04 11:44:20 +0000

    given single number fail

--- Calculator.Tests/StringCalculatorTests.cs
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -3,6 +3,8 @@ using Xunit;
 
 namespace Calculator.Tests
 {
+    using AutoFixture.Xunit2;
+
     public class StringCalculatorTests
     {
         [Fact]
@@ -10,6 +12,14 @@ namespace Calculator.Tests
         {
             var result = new StringCalculator().Add(string.Empty);
             Assert.Equal(0, result);
+        }
+
+        [Theory, AutoData]
+        public void Given_a_single_number_should_return_the_number(StringCalculator sut, int number)
+        {
+            var inputString = $"{number}";
+            var result = sut.Add(inputString);
+            Assert.Equal(number, result);
         }
     }
 }
