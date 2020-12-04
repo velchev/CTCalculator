a355ead49e79b5ee338531686ae0b766e8abae6c
Lyubomir Velchev <lyubomir.velchev@gmail.com>
2020-12-04 12:16:19 +0000

    new line as a delimiter afte comma test

--- Calculator.Tests/StringCalculatorTests.cs
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -55,5 +55,13 @@ namespace Calculator.Tests
 
             Assert.Equal(integersToTest.Sum(), result);
         }
+
+        [Theory, AutoData]
+        public void Given_new_line_as_a_delimiter_after_comma_should_return_format_exception(StringCalculator sut)
+        {
+            string inputString = "1,\n";
+            var exception = Assert.Throws<FormatException>(() => sut.Add(inputString));
+            Assert.Contains("Two consecutive delimiters are not allowed", exception.Message);
+        }
     }
 }
