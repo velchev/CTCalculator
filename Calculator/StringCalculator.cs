3a524e6462741e25fb4d230ba530e6f9bfadc289
Lyubomir Velchev <lyubomir.velchev@gmail.com>
2020-12-04 11:40:56 +0000

    empty string green test

--- Calculator/StringCalculator.cs
+++ Calculator/StringCalculator.cs
@@ -6,7 +6,13 @@ namespace Calculator
     {
         public int Add(string numbers)
         {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
             return -1;
+
         }
     }
 }
